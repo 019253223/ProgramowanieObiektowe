97e693c042dde7760244b293e19b33e33ea23469
019253223 <62806375+019253223@users.noreply.github.com>
2020-04-02 20:09:04 +0200

    Add files via upload

---
+++ lab1/tablica.h
@@ -0,0 +1,10 @@
+#ifndef _TABLICA_H
+#define _TABLICA_H
+
+void zapis_tablica (int rozmiar, int kolumny, int **arkusz);
+
+int wyswietlanie_tablica (int **arkusz);
+
+void wpisz_zawartosc (int **arkusz, int rozmiar, int kolumny);
+
+#endif
