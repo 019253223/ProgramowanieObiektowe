75ec9e6d3318bb7da7d718518390cb36d65eca54
019253223 <253223@student.pwr.edu.pl>
2020-04-02 20:32:08 +0200

    lab1

---
+++ tablica.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#include"tablica.h"
+
+void zapis_tablica (int rozmiar, int kolumny, int **arkusz) {
+
+    cout<<"Podaj liczbe wierszy: ";
+    cin>>rozmiar;
+
+
+    for( int i = 1; i <= rozmiar; i++ ) {
+        cout<<"Podaj liczbe kolumn w "<< i << " wierszu: ";
+        cin>>kolumny;
+        arkusz[i] = new int[kolumny];    // alokacja tablicy
+
+    for(int j=1; j <= kolumny; j++) {
+        cout<<"Wpisz do komorki "<< j << " wartosc : ";
+        cin>>arkusz[i][j];              //wpisywanie liczby do tablicy
+    }
+    }
+}
+
+
+int wyswietlanie_tablica (int **arkusz){
+
+
+	//return ;
+}
+
+
+void wpisz_zawartosc (int **arkusz, int rozmiar, int kolumny) {
+
+}
