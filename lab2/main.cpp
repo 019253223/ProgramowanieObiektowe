263ae62b0cab2a86b75e69e38ad6963ccf9e1898
019253223 <253223@student.pwr.edu.pl>
2020-05-17 16:54:24 +0200

    try

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -9,19 +9,20 @@ using namespace std;
 int main () {
 
   	int wybor;
-	int ilosc_wierszy = 1000;
-	int ilosc_kolumn = 1000;
+	int ilosc_wierszy = 100 ;
+	int ilosc_kolumn = 100 ;
  	int **arkusz = new int * [ilosc_wierszy];
 	
 	for (int i = 0; i < ilosc_wierszy; i++) {
 	     arkusz[i] = new int [ilosc_kolumn];
 	}
 
-	/* cout << "zarezerwowano: " << ilosc_wierszy << endl;
-	cout<< "sizeof: " << sizeof(arkusz) << endl; */
-    
-	petla_menu(arkusz, wybor, ilosc_wierszy, ilosc_kolumn);
+	 
+    cout << "zarezerwowano: " << ilosc_wierszy << endl;
+	cout<< "sizeof: " << sizeof(arkusz) << endl;
 
+	petla_menu(arkusz, wybor, ilosc_wierszy, ilosc_kolumn);
+	
 
 	for (int i = 0; i < ilosc_wierszy; i++) {
 	    delete [] arkusz[i];	    
